5a24f0b03fc8dd863fa6588fce29bb6c43ce3068
gnarmeister <sb9155118@naver.com>
2022-05-27 10:55:11 +0900

    start discord.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from pydoc import cli
+from bot import client
+
+TOKEN = "ODk3Mzk4OTU5MzAyMTMxNzEy.GfgAj0.Yt9nPxfaEN213_oR5ovY1BNAPf8A9_ulEKpSHo"
+
+
+def main():
+    client.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
