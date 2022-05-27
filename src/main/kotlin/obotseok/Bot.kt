d667cf2b59749622950c207a8659ef7a9d56773c
gnarmeister <sb9155118@naver.com>
2022-05-27 10:17:23 +0900

    minor update

--- src/main/kotlin/obotseok/Bot.kt
+++ src/main/kotlin/obotseok/Bot.kt
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.JDABuilder
 import obotseok.command.Island
 import obotseok.command.Ping
 import obotseok.listner.EventListener
-
+ 
 class Bot(private val token: String) {
     val commands = listOf(
         Ping(),
