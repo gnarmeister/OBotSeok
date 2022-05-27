1efa0c9bfdc200cad6514cb4992122e30b3b847b
gnarmeister <sb9155118@naver.com>
2022-05-27 10:22:16 +0900

    remove legacy files

--- src/main/kotlin/obotseok/command/Ping.kt
+++
@@ -1,9 +0,0 @@
-package obotseok.command
-
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
-
-class Ping : Command("!ping") {
-    override fun run(event: GuildMessageReceivedEvent) {
-        event.channel.sendMessage("Pong").queue()
-    }
-}
