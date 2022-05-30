7a18838b2d0e7cfb0d8b940e8518cc0de2ce4854
gnarmeister <sb9155118@naver.com>
2022-05-30 10:34:43 +0900

    testing voice

--- bot/handle_command.py
+++ bot/handle_command.py
@@ -12,9 +12,16 @@ async def command_help(message: discord.Message):
     await message.channel.send(s)
 
 
+async def command_sylvanas(message: discord.Message):
+    voice_channel = message.guild.voice_channels[0]
+    voice_client = await voice_channel.connect()
+    print(voice_client)
+
+
 command_dict = {
     "깃허브": {"function": command_github, "description": "깃허브 저장소 주소"},
     "도움말": {"function": command_help, "description": "도움말"},
+    "실바나스": {"function": command_sylvanas, "description": "오범석의 성우 데뷔작"},
 }
 
 
