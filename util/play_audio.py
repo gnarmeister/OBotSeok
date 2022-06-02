f459363281adbda81c2a3225faec45b4e7c52eb5
gnarmeister <sb9155118@naver.com>
2022-06-02 23:35:44 +0900

    change type of audio source

--- util/play_audio.py
+++ util/play_audio.py
@@ -10,21 +10,16 @@ async def play_audio(filename: str, message: discord.Message):
     for i in range(len(channels)):
         if len(channels[i].members) > 0:
             voice_channel = channels[i]
-    message.channel.send("channel found")
 
     # connect to voice channel
     voice_client: discord.VoiceClient = await voice_channel.connect()
-    message.channel.send("connected")
 
     # play audio file through FFmpeg
     src_path = os.path.join(os.getcwd(), "sound", filename)
-    message.channel.send(src_path)
-    source: discord.AudioSource = discord.FFmpegPCMAudio(src_path)
+    source: discord.FFmpegPCMAudio = discord.FFmpegPCMAudio(src_path)
     voice_client.play(source)
-    message.channel.send("playing")
 
     # disconnect after playing
     while voice_client.is_playing():
         time.sleep(1)
     await voice_client.disconnect()
-    message.channel.send("done")
